6f9e0df1482eaa2201e294a5c0da56d117a9a84f
vinay <vinay@DESKTOP-VVA28O4>
2019-12-02 17:43:20 +0530

    commit

--- github/src/bean/User.java
+++ github/src/bean/User.java
@@ -3,6 +3,31 @@ package bean;
 public class User {
  private int id;
  private String firstName;
+	private String Sudha;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSudha() {
+		return Sudha;
+	}
+
+	public void setSudha(String sudha) {
+		Sudha = sudha;
+	}
  
  
 }
